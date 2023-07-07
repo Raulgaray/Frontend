f2b72a3ba32f9aabc19faf04e8fe3fbbe937b641
Raulgaray <85389355+Raulgaray@users.noreply.github.com>
2023-07-07 00:30:45 -0500

    Commit Frontend

---
+++ src/components/user/Register.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useForm } from "../../hooks/useForm";
+
+export const Register = () => {
+
+  const {form, changed} = useForm({});
+
+  const saveUser = (e) => {
+    e.preventDefault()
+
+    let newUser = form;
+
+    console.log(newUser)
+  }
+
+  return (
+    <>
+      <header className="content__header content__header--public">
+        <h1 className="content__title">Registro</h1>
+      </header>
+
+      <div className="content__posts">
+
+        <form className="register-form" onSubmit={saveUser}>
+
+          <div className="form-group">
+            <label htmlFor="name">Nombre</label>
+            <input type="text" name="name" onChange={changed} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="surname">Apellidos</label>
+            <input type="text" name="surname" onChange={changed} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="nick">Nick</label>
+            <input type="text" name="nick" onChange={changed} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Correo Electronico</label>
+            <input type="email" name="email" onChange={changed} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" name="password" onChange={changed} />
+          </div>
+
+          <input type="submit" value="Registrate" className="btn btn-success" />
+        </form>
+
+      </div>
+    </>
+  );
+};
